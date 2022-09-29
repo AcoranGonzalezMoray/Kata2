590052286ebfc5369c0eddecda9abee52bddef82
AcoranGonzalezMoray <acoranlol14@gmail.com>
2022-09-29 09:20:32 +0100

    Kata2 versión 1

---
+++ src/kata2/Kata2.java
@@ -0,0 +1,25 @@
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Kata2 {
+
+    public static void main(String[] args) {
+        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
+        int[] data = {1,2,3,4,1,2,4,6,7,8,2, 8, 6, 6, 7};
+        
+        for (int i = 0; i < data.length; i++) {
+          if(histogram.containsKey(data[i])) histogram.put(data[i], histogram.get(data[i])+1); 
+          else histogram.put(data[i], 1);
+        }
+        
+        for(Map.Entry<Integer, Integer> entry: histogram.entrySet()){
+            System.out.println(entry.getKey() + " ==> " + entry.getValue());
+        }
+        
+        
+        
+    }
+    
+}
